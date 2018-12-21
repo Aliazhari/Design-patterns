e33b12aecc2cbd0420efae2a27caa4d959e9567a
Aliazhari <Aliazhari@hotmail.com>
2018-12-21 06:29:27 -0500

    Setting up env

---
+++ Strategy/src/main/java/Strategy/Strategy/App.java
@@ -0,0 +1,13 @@
+package Strategy.Strategy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
