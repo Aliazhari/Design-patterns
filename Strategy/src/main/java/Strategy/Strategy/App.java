e5924ca90a2a6d1cfc6e6c62acd8a621233160a3
Aliazhari <Aliazhari@hotmail.com>
2018-12-21 06:40:38 -0500

    Adding strategy operations

--- Strategy/src/main/java/Strategy/Strategy/App.java
+++ Strategy/src/main/java/Strategy/Strategy/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	 Context context = new Context(new OperationAdd());		
+         System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+         context = new Context(new OperationSubstract());		
+         System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+
+         context = new Context(new OperationMultiply());		
+         System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
     }
 }
---
+++ Strategy/src/main/java/Strategy/Strategy/OperationMultiply.java
@@ -0,0 +1,10 @@
+package Strategy.Strategy;
+
+public class OperationMultiply implements Strategy {
+
+	public int doOperation(int num1, int num2) {
+		
+		return num1 * num2;
+	}
+
+}
